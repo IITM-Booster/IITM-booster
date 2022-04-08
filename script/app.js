8d5fe20a1bce73da19149d90f6d12079d068c470
dibakash <44256849+dibakash@users.noreply.github.com>
2022-04-08 11:30:35 +0530

    added JavaScript boilerplate (#4)
    Co-authored-by: dibakash <dibakash@users.noreply.github.com>

---
+++ script/app.js
@@ -0,0 +1,30 @@
+"use strict";
+
+; (function () {
+    // load functions
+    if (document.readyState == "loading") {
+        document.addEventListener("DOMContentLoaded", function () {
+            console.log("Running script with DOMContentLoaded method");
+            let app = new iitm_app();
+            app.init();
+
+        });
+    } else {
+        console.log("Running script with defer attribute method");
+        setTimeout(() => {
+            let app = new iitm_app();
+            app.init();
+        }, 0);
+
+    }
+
+    // class
+    class iitm_app {
+
+        init() {
+            console.log(this);
+        }
+
+    };
+
+})();
